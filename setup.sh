6ffa18b25d125928f10d2471c562e60c054f4e6b
Ryan Skoblenick <ryan@skoblenick.com>
2013-10-26 12:51:26 -0400

    Complete refactor of installation script.
    - Removed dependency on config.js and json parse (js)

--- setup.sh
+++ setup.sh
@@ -7,127 +7,117 @@
 # Prompt for sudo password upfront
 sudo -v
 
-os_family() {
+function install () {
+    local url=${1}
+
+    # Parse package details from url
+    local filename="${url##*/}"
+    local extension="${filename##*.}"
+    local file="${filename%.${extension}}"
+    local package="${file%%-*}"
+    local version="${file#*-*}"
+    local version="${version%*-*}"
+
+    # Validate the command doesn't exist
+    command -v $package >/dev/null 2>&1
+
+    # Get the installed version
+    if [ $? -eq 0 ]; then
+        version_present=$( $package --version )
+    else
+        version_present=0
+    fi
+
+    # Valifate if we need to install by version checking present to requested
+    version_compare "$version_present" "$version"
+    case $? in
+        #0) echo "force reinstall";;
+        #1) echo "downgrade?";;
+        2) # Install
+            http_code=$( curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url )
+            if [ "${http_code}" -eq '200' ]; then
+                curl -L -o "/tmp/${filename}" --url "${url}"
+            fi
+
+            if [ -e "/tmp/$filename" ]; then
+                case ${extension} in
+                    'dmg')
+                        # Mount disk image
+                        hdiutil attach -nobrowse -readonly -noidme "/tmp/${filename}" -mountpoint "/Volumes/${filename}"
+                        
+                        # Identify the pkg installer within the mounted volume
+                        pkg=$( ls "/Volumes/${filename}" | grep '.pkg' )
+                        sudo installer -pkg "/Volumes/${filename}/${pkg}" -target /
+
+                        # Unmount disk image
+                        hdiutil detach "/Volumes/${filename}"
+                    ;;
+                    *)
+                        echo "Unsupported package installer!"
+                        exit 9
+                    ;;
+                esac
+
+                # Clean up
+                rm -f "/tmp/${filename}"
+            fi
+        ;;
+    esac
+
+}
+
+function main () {
     case $( uname -s ) in
         Darwin)
-            echo "Darwin"
-        ;;
-        Linux)
-            case $( lsb_release -si ) in
-                Debian|Ubuntu)
-                    echo "Debian"
-                ;;
-                RedHat|CentOS)
-                    echo "RedHat"
-                ;;
-                *)
-                echo "Unsupported Linux distribution."
-                exit 9
-            esac
+            install 'http://downloads.puppetlabs.com/mac/facter-1.7.3.dmg'
+            install 'http://downloads.puppetlabs.com/mac/hiera-1.2.1.dmg'
+            install 'http://downloads.puppetlabs.com/mac/puppet-3.3.1.dmg'
+
+            # Ensure puppet group is present
+            sudo puppet resource group puppet ensure=present > /dev/null
+
+            # Ensure puppet user is present
+            sudo puppet resource user puppet ensure=present gid=puppet shell="/sbin/nologin" 2>&1 > /dev/null
+
+            # Hide the puppet user; otherwise it will appear on the login screen
+            defaults read /Library/Preferences/com.apple.loginwindow HiddenUsersList 2>&1 | grep --quiet "puppet"
+            if [ $? -ne 0 ]; then
+                sudo defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add puppet
+            fi
         ;;
         *)
             echo "Unsupported operating system."
             exit 9
+        ;;
     esac
+
+    exit 0
 }
 
-install_deps() {
-    local config=${1}
-
-    install_jq
-
-    if [ ! -f "${config}" ]; then
-        echo 'No config found.'
-        exit 1
+function version_compare () {
+    # http://stackoverflow.com/a/4025065/1837034
+    if [[ $1 == $2 ]]; then
+        return 0
     fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
 
-    if [ $( os_family ) == "Darwin" ]; then
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
+        ver1[i]=0
+    done
 
-        packages=($(cat "${config}" | jq -r -c '.dependencies[].source[] | select(.platform == "Darwin").uri'))
-
-        for package in "${packages[@]}"
-        do
-            # Extract the filename from the URL
-            filename=$( basename ${package} )
-
-            # Download
-            curl -L -o "/tmp/${filename}" ${package}
-
-
-            # Mount disk image
-            hdiutil attach -nobrowse -readonly -noidme "/tmp/${filename}" -mountpoint "/Volumes/${filename}"
-
-            # Install
-            pkg=$( ls "/Volumes/${filename}" | grep '.pkg' )
-            sudo installer -pkg "/Volumes/${filename}/${pkg}" -target /
-
-            # Unmount disk image
-            hdiutil detach "/Volumes/${filename}"
-
-            # Clean up
-            rm -f "/tmp/${filename}"
-        done
-
-        # Ensure puppet group is present
-        sudo puppet resource group puppet ensure=present
-
-        # Ensure puppet user is present
-        sudo puppet resource user puppet ensure=present gid=puppet shell="/sbin/nologin"
-
-        # Hide the puppet user; otherwise it will appear on the login screen
-        sudo defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array-add puppet
-    else
-        echo "Feature not implemented"
-        exit 2
-    fi
-}
-
-install_jq() {
-    # Install Command Line JSON parser
-    # http://stedolan.github.io/jq/
-    local os=''
-
-    if [ ! -f /usr/bin/jq ]; then
-        if [ $( os_family ) == "Darwin" ]; then
-            os='osx64'
-        else
-            os='linux64'
+    for ((i=0; i<${#ver1[@]}; i++)); do
+        if [[ -z ${ver2[i]} ]]; then
+            ver2[i]=0
         fi
-        sudo curl -L -o '/usr/bin/jq' "http://stedolan.github.io/jq/download/${os}/jq"
-        sudo chmod a+x '/usr/bin/jq'
-        echo 'Command Line JSON parser installed...'
-    else
-        echo 'Command Line JSON parser already installed... skipping...'
-    fi
+        if ((10#${ver1[i]} > 10#${ver2[i]})); then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]})); then
+            return 2
+        fi
+    done
+    return 0
 }
 
-usage() {
-    echo "Usage: $0 [-c]" 1>&2; exit 1;
-}
-
-
-# Parse getopts
-optspec=":hc-:"
-
-while getopts "${optspec}" optchar; do
-    case "${optchar}" in
-        -)
-            case "${OPTARG}" in
-                config)
-                    echo "Feature not implemented"
-                    exit 2
-                    ;;
-            esac;;
-        c)
-            val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-            #echo "Parsing option: '-${optchar}', value: '${val}' " >&2
-            install_deps ${val}
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
-shift $((OPTIND-1))
-
-exit 0
+main
